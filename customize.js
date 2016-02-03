3578fcb20ed2ea789ca8fb1eeceb50f5bb7a1bfe
viveck1161 <viveck1313@gmail.com>
2016-02-03 14:54:14 +0530

    fir

---
+++ customize.js
@@ -0,0 +1,17 @@
+ $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+$(document).ready(function(){
+	$('.collapsible').collapsible({
+			accordin: true
+	});
+	
+});
+
+$(document).ready(function(){               
+	  $('.materialboxed').materialbox();
+	});
+	$(document).ready(function(){
+	  $('.materialboxed').materialbox();
+	});
